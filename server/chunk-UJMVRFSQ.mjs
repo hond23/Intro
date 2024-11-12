e9a7ffa9cbfb27ad734874e103abd8507c056f8c
Han <hond2313@gmail.com>
2024-11-11 20:09:33 -0500

    Auto-generated commit

---
+++ server/chunk-UJMVRFSQ.mjs
@@ -0,0 +1,166 @@
+import './polyfills.server.mjs';
+import{B as v,a as l,b as a,c as o,d as e,e as i,f as r,g as c,h as p,o as g,q as m,r as s,u as d,y as h}from"./chunk-AE2Y55DT.mjs";var f=(()=>{class n{constructor(){this.title="Han Intro"}static{this.\u0275fac=function(t){return new(t||n)}}static{this.\u0275cmp=l({type:n,selectors:[["app-root"]],standalone:!0,features:[p],decls:9,vars:1,consts:[[1,"main"],[1,"content"],[1,"left-side"],["role","separator","aria-label","Divider",1,"divider"]],template:function(t,P){t&1&&(o(0,"main",0)(1,"div",1)(2,"div",2)(3,"h1"),r(4),e(),o(5,"p"),r(6,"Congratulations! Your app is running. \u{1F389}"),e()(),i(7,"div",3),e()(),i(8,"router-outlet")),t&2&&(a(4),c("Hello, ",P.title,""))},dependencies:[h],styles:[`[_nghost-%COMP%] {
+    --bright-blue: oklch(51.01% 0.274 263.83);
+    --electric-violet: oklch(53.18% 0.28 296.97);
+    --french-violet: oklch(47.66% 0.246 305.88);
+    --vivid-pink: oklch(69.02% 0.277 332.77);
+    --hot-red: oklch(61.42% 0.238 15.34);
+    --orange-red: oklch(63.32% 0.24 31.68);
+
+    --gray-900: oklch(19.37% 0.006 300.98);
+    --gray-700: oklch(36.98% 0.014 302.71);
+    --gray-400: oklch(70.9% 0.015 304.04);
+
+    --red-to-pink-to-purple-vertical-gradient: linear-gradient(
+      180deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --red-to-pink-to-purple-horizontal-gradient: linear-gradient(
+      90deg,
+      var(--orange-red) 0%,
+      var(--vivid-pink) 50%,
+      var(--electric-violet) 100%
+    );
+
+    --pill-accent: var(--bright-blue);
+
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1[_ngcontent-%COMP%] {
+    font-size: 3.125rem;
+    color: var(--gray-900);
+    font-weight: 500;
+    line-height: 100%;
+    letter-spacing: -0.125rem;
+    margin: 0;
+    font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+
+  p[_ngcontent-%COMP%] {
+    margin: 0;
+    color: var(--gray-700);
+  }
+
+  main[_ngcontent-%COMP%] {
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    box-sizing: inherit;
+    position: relative;
+  }
+
+  .angular-logo[_ngcontent-%COMP%] {
+    max-width: 9.2rem;
+  }
+
+  .content[_ngcontent-%COMP%] {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    max-width: 700px;
+    margin-bottom: 3rem;
+  }
+
+  .content[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+    margin-top: 1.75rem;
+  }
+
+  .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+    margin-top: 1.5rem;
+  }
+
+  .divider[_ngcontent-%COMP%] {
+    width: 1px;
+    background: var(--red-to-pink-to-purple-vertical-gradient);
+    margin-inline: 0.5rem;
+  }
+
+  .pill-group[_ngcontent-%COMP%] {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    flex-wrap: wrap;
+    gap: 1.25rem;
+  }
+
+  .pill[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    --pill-accent: var(--bright-blue);
+    background: color-mix(in srgb, var(--pill-accent) 5%, transparent);
+    color: var(--pill-accent);
+    padding-inline: 0.75rem;
+    padding-block: 0.375rem;
+    border-radius: 2.75rem;
+    border: 0;
+    transition: background 0.3s ease;
+    font-family: var(--inter-font);
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.4rem;
+    letter-spacing: -0.00875rem;
+    text-decoration: none;
+  }
+
+  .pill[_ngcontent-%COMP%]:hover {
+    background: color-mix(in srgb, var(--pill-accent) 15%, transparent);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 1) {
+    --pill-accent: var(--bright-blue);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 2) {
+    --pill-accent: var(--french-violet);
+  }
+  .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 3), .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 4), .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 5) {
+    --pill-accent: var(--hot-red);
+  }
+
+  .pill-group[_ngcontent-%COMP%]   svg[_ngcontent-%COMP%] {
+    margin-inline-start: 0.25rem;
+  }
+
+  .social-links[_ngcontent-%COMP%] {
+    display: flex;
+    align-items: center;
+    gap: 0.73rem;
+    margin-top: 1.5rem;
+  }
+
+  .social-links[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    transition: fill 0.3s ease;
+    fill: var(--gray-400);
+  }
+
+  .social-links[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover   svg[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+    fill: var(--gray-900);
+  }
+
+  @media screen and (max-width: 650px) {
+    .content[_ngcontent-%COMP%] {
+      flex-direction: column;
+      width: max-content;
+    }
+
+    .divider[_ngcontent-%COMP%] {
+      height: 1px;
+      width: 100%;
+      background: var(--red-to-pink-to-purple-horizontal-gradient);
+      margin-block: 1.5rem;
+    }
+  }`]})}}return n})();var u=[];var C={providers:[v(u),s()]};var _={providers:[d()]},M=g(C,_);var b=()=>m(f,M),B=b;export{B as a};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UJMVRFSQ.mjs";import"./chunk-AE2Y55DT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
